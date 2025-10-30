51375bfd469ec3443f09006d471cf09b54133953
Rodas <manurodaspinilla@gmail.com>
2025-10-30 14:09:58 +0100

    ahora si

--- script.js
+++ script.js
@@ -2,18 +2,15 @@ const lightBtn = document.getElementById('lightBtn');
 const darkBtn = document.getElementById('darkBtn');
 const body = document.body;
 
-// Cargar el tema guardado o por defecto claro
-const savedTheme = localStorage.getItem('theme') || 'light-mode';
+const savedTheme = localStorage.getItem('theme') || 'dark-mode';
 body.classList.add(savedTheme);
 
-// Modo claro
 lightBtn.addEventListener('click', () => {
     body.classList.remove('dark-mode');
     body.classList.add('light-mode');
     localStorage.setItem('theme', 'light-mode');
 });
 
-// Modo oscuro
 darkBtn.addEventListener('click', () => {
     body.classList.remove('light-mode');
     body.classList.add('dark-mode');
